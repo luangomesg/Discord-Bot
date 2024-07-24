c33ea0426e33d9e81d82ff0158e9881e3171327c
Luan <luanitachi@hotmail.com>
2024-07-23 23:24:45 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { Options } from 'discord.js'
+import { ExtendedClient } from './structs/ExtendedClient'
+export * from "colors"
+import config from './config.json'
+
+
+
+const client = new ExtendedClient()
+
+client.start()
+
+export { client, config }
+
